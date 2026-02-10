bca457786d595f0535ecd7121c55ca27e9b8ccad
VesperQuartz <lilbrown3000@gmail.com>
2026-02-10 16:22:29 +0100

    add credentials include

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function proxy() {
+export default function proxy() {
 	const res = NextResponse.next();
 
 	res.headers.append("Access-Control-Allow-Credentials", "true");
